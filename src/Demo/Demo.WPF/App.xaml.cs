94674fe87e68561002b36c6c9186c2aaa5623de4
Alexandr Vassilyev <alexandr_vslv@mail.ru>
2019-10-10 18:39:05 +0600

    Update Packages and Dwmo.WPF project

--- src/Demo/Demo.WPF/App.xaml.cs
+++ src/Demo/Demo.WPF/App.xaml.cs
@@ -13,5 +13,11 @@ namespace Demo.WPF
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            MainWindow = new MainWindow();
+            MainWindow.Show();
+        }
+
     }
 }
